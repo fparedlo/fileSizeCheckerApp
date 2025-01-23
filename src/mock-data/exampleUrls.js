a1a5f4be5f832cf2891e509c039ffdb6940203e5
F Paredes <fparedlo@gmail.com>
2025-01-23 23:32:42 +0000

    updated urls from mock data avoid problems

--- src/mock-data/exampleUrls.js
+++ src/mock-data/exampleUrls.js
@@ -1,7 +1,7 @@
 const exampleUrls = [
-  "https://resources.mandmdirect.com/assets/homepage/panels/250120/LD19_HPM_desktop.webp",
-  "https://resources.mandmdirect.com/assets/roundels/2024/Quicklinks/241219/IE/roundel-new-in_desktop.jpg",
-  "https://resources.mandmdirect.com/assets/roundels/2024/Quicklinks/241219/IE/roundel-mens_desktop.jpg",
+  "https://www.boredpanda.com/blog/wp-content/uploads/2024/10/67176d9793d62_random-funny-no-context-pics.jpg",
+  "https://www.boredpanda.com/blog/wp-content/uploads/2024/10/pfbid02W3Tz4Peh2gUfxcrw9trqSw2fda2TaYXgkDESjcSMYYfLb1keXTU8dEk339c8EdeVlrefembed_post-png__700.jpg",
+  "https://www.boredpanda.com/blog/wp-content/uploads/2024/10/450807003_1027743112248417_8679963896665622565_n-67176400b71dd__700-67176c0631ae5__700.jpg",
 ];
 
 export default exampleUrls;
