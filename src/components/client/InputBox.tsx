295f5282b1f987b4fbc24607c60779c877184505
fparedlo <fparedlo@gmail.com>
2025-01-23 15:39:09 +0000

    reordering test

--- src/components/client/InputBox.tsx
+++ src/components/client/InputBox.tsx
@@ -2,7 +2,12 @@ export default function InputBox() {
       return (
          <label className="grid">
                Copy paste your file URLs here.
-               <textarea className="border" id="fileCheckerTextArea" cols={2} rows={10} />
+               <textarea 
+                  className="border p-3" 
+                  id="fileCheckerTextArea"
+                  name="fileCheckerTextArea"
+                  cols={2}
+                  rows={10} />
          </label>
       )
 }
