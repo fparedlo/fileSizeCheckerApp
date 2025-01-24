9923509a8ab78ae836a5d589783cc23a17c9746d
F Paredes <fparedlo@gmail.com>
2025-01-24 09:56:15 +0000

    reduced url link size and improved example spacing

--- src/mock-data/exampleUrls.js
+++ src/mock-data/exampleUrls.js
@@ -1,7 +1,7 @@
 const exampleUrls = [
-  "https://www.boredpanda.com/blog/wp-content/uploads/2024/10/67176d9793d62_random-funny-no-context-pics.jpg",
-  "https://www.boredpanda.com/blog/wp-content/uploads/2024/10/pfbid02W3Tz4Peh2gUfxcrw9trqSw2fda2TaYXgkDESjcSMYYfLb1keXTU8dEk339c8EdeVlrefembed_post-png__700.jpg",
-  "https://www.boredpanda.com/blog/wp-content/uploads/2024/10/450807003_1027743112248417_8679963896665622565_n-67176400b71dd__700-67176c0631ae5__700.jpg",
+  "https://shorturl.at/jGoFC",
+  "https://shorturl.at/MGCkv",
+  "https://shorturl.at/cjoRn",
 ];
 
 export default exampleUrls;
