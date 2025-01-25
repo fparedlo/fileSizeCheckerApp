b95e95fca18872d6f931f36ca3d3e74e7207d86a
F Paredes <fparedlo@gmail.com>
2025-01-25 20:14:26 +0000

    added a 3s delay on each request to try to mitigate the load on target servers

--- src/utils/fetchFileSizes.ts
+++ src/utils/fetchFileSizes.ts
@@ -1,31 +1,37 @@
 import type { FileSize, Urls } from "@/types/app.types";
 const CORS_PROXY = "https://cors-anywhere.herokuapp.com";
 
+const delay = (ms: number): Promise<void> =>
+	new Promise((resolve) => setTimeout(resolve, ms));
+
 async function fetchFileSizes(urls: Urls): Promise<FileSize[]> {
-	const fileSizes = await Promise.all(
-		urls.map(async (url) => {
-			try {
-				const response = await fetch(`${CORS_PROXY}/${url}`, {
-					method: "HEAD",
-				});
-				if (!response.ok) {
-					throw new Error(`Error fetching ${url}: ${response.statusText}`);
-				}
-				const contentLength = response.headers.get("Content-Length");
-				return {
-					url: url,
-					size: contentLength
-						? `${(Number.parseInt(contentLength, 10) / 1024).toFixed(2)} kB`
-						: "Unknown size",
-				};
-			} catch (error) {
-				return {
-					url: url,
-					size: `Error: ${(error as Error).message}`,
-				};
+	const fileSizes: FileSize[] = [];
+
+	for (const url of urls) {
+		try {
+			const response = await fetch(`${CORS_PROXY}/${url}`, {
+				method: "HEAD",
+			});
+			if (!response.ok) {
+				throw new Error(`Error fetching ${url}: ${response.statusText}`);
 			}
-		}),
-	);
+			const contentLength = response.headers.get("Content-Length");
+			fileSizes.push({
+				url: url,
+				size: contentLength
+					? `${(Number.parseInt(contentLength, 10) / 1024).toFixed(2)} kB`
+					: "Unknown size",
+			});
+		} catch (error) {
+			fileSizes.push({
+				url: url,
+				size: `Error: ${(error as Error).message}`,
+			});
+		}
+
+		// Wait for 3 seconds before the next request
+		await delay(3000);
+	}
 
 	return fileSizes;
 }
