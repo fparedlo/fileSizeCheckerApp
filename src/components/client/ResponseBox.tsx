1e585f56af7708052f6a157bca965f3102b4ede2
F Paredes <fparedlo@gmail.com>
2025-01-25 23:00:07 +0000

    Improved results ui

--- src/components/client/ResponseBox.tsx
+++ src/components/client/ResponseBox.tsx
@@ -4,13 +4,13 @@ export default function ResponseBox({ data }: { data: FileSize[] | null }) {
 	return (
 		<div>
 			{data ? (
-				<ul className="grid gap-4 mt-4 grid-cols-3">
+				<ul className="grid gap-4 mt-4 grid-cols-3 ps-0">
 					{data.map((file) => (
 						<li
 							key={file.url}
 							className="grid gap-2 p-4 bg-gray-50 border border-gray-200 rounded-lg font-mono"
 						>
-							<span className="text-lg break-all">{file.url}</span>
+							<span className="text-sm break-all">{file.url}</span>
 							<span className="text-2xl text-right break-all">
 								<strong>{file.size}</strong>
 							</span>
